cb29e826eff0d6fef6cea928fd4960bc876ea112
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-11-22 16:48:49 +0100

    Aggiunto Faker, c'è qualche problema

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -4,14 +4,23 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class RestaurantSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $restaurant = [
+                $name=> $faker->$name,
+                $address=> $faker->$address,
+                $BusinessNumber=> $faker->$BusinessNumber
+            ];
+
+            $newRestaurant = Restaurant::create($restaurant);
+        }
     }
 }
