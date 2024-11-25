22d45848f4c18ba073ce08f669d53402ebfddb77
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-11-25 16:41:29 +0100

    create le relative table presenti dentro il diagramma

---
+++ app/Models/Utent.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Utent extends Model
+{
+    use HasFactory;
+}
