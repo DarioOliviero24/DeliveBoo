83269f82d52a32f87263a7f833072620c115b667
Francescorussoo <164784843+Francescorussoo@users.noreply.github.com>
2024-11-27 10:14:25 +0100

    aggiornamento platesseeder

--- database/seeders/PlatesSeeder.php
+++ database/seeders/PlatesSeeder.php
@@ -1,8 +1,7 @@
-<?php
+<?php 
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Plates;
 use Faker\Generator as Faker;
@@ -12,26 +11,28 @@ class PlatesSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-
-
     public function run(Faker $faker): void
     {
         $ingredients = [
-            'Sugar',
-            'Flour',
+            'Sugar', 
+            'Flour', 
             'Salt',
             'Butter',
-            'Milk',
+            'Milk', 
             'Eggs',
-            'Cheese',
+            'Cheese', 
             'Tomatoes'
+            
         ];
 
         for ($i = 0; $i < 10; $i++) {
             $plates = [
-                'name' => $faker->name,
+                'name' => $faker->word,
                 'ingredient' => $ingredients[array_rand($ingredients)],
-                'price' => $faker->randomFloat(2, 1, 1000),
+                'price' => $faker->randomFloat(2, 5, 50),
+                'restaurants_id' => $faker->numberBetween(1, 5),
+                'created_at' => now(),
+                'updated_at' => now(),
             ];
 
             Plates::create($plates);
