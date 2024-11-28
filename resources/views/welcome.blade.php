578a674a858ede0fe3226670f4b2a1c612b8392f
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-11-29 00:05:52 +0100

    cancellate modifiche per un errore che non riuscivo a risolvere

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.guest')
 
 @section('main-content')
-    <div style="background-color: #F5FFFA; padding: 20px; border-radius: 15px; box-shadow: 0 6px 12px rgba(0, 0, 0, 0.1);">
-        <h1 class="text-center" style="color: black; font-size: 2.5rem;">
-            DELIVEBOO
+    <div style="background-color: #f0f8ff; padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+        <h1 class="text-center" style="color: #007bff; font-size: 2.5rem;">
+            DeliveBoo
         </h1>
         <h2 style="color: #6c757d; font-size: 2rem;">
             @foreach ($restaurants as $restaurant)
                 <div style="background-color: #ffffff; margin: 10px 0; padding: 15px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
-                    <h5 style="color: black; font-size: 2.5rem;" class="card-title ">{{ $restaurant->name }}</h5>
-                    <p style="color: grey" class="card-text">{{ $restaurant->address }}</p>
-                    <a href="{{ route('welcomeLoggato.show', $restaurant->id) }}" class="btn btn-outline-success">Vedi Piatti</a>
+                    <p style="color: #28a745; font-size: 1.5rem;">{{ $restaurant->name }}</p>
+                    <a href="{{ route('home.show', $restaurant->id) }}" class="btn btn-primary" style="background-color: #007bff; border-color: #007bff;">Show</a>
                 </div>
+                
 
             @endforeach
         </h2>
