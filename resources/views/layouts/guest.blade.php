ca4c91ad4e25e0f0c6d64f78aadb05c2dec4e2d6
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-11-29 01:46:29 +0100

    Modificati Home, Login , Register ( da sloggati)

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,12 +14,15 @@
         <header>
             <nav class="navbar navbar-expand-lg bg-white">
                 <div class="container">
-                    <a class="navbar-brand" href="/">Home</a>
+                    <a class="navbar-brand" href="/">
+                        <button type="button" class="btn btn-success">Home</button>
+                    </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
+
                     <div class="collapse navbar-collapse" id="navbarText">
-                        
+
                         @auth
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
@@ -32,10 +35,14 @@
                         @else
                             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">Login</a>
+                                    <a class="nav-link" href="{{ route('login') }}">
+                                        <button type="button" class="btn btn-warning">Login</button>
+                                    </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">Register</a>
+                                    <a class="nav-link" href="{{ route('register') }}">
+                                        <button type="button" class="btn btn-danger">Register</button>
+                                    </a>
                                 </li>
                             </ul>
                         @endauth
@@ -57,7 +64,7 @@
             <div class="container">
                 @yield('main-content')
 
-                
+
             </div>
         </main>
     </body>
