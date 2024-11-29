74d4f23ef255bf1bc802ef64cdc51158c34a5cb9
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-11-29 16:35:20 +0100

    Modificato register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,45 +5,57 @@
         @csrf
 
         <!-- Name -->
-        <div>
+        <div class="text-center py-2">
             <label for="name">
-                Name
+                <h3>
+                    Nome
+                </h3>
             </label>
-            <input type="text" id="name" name="name">
+            <input class="form-control me-2" placeholder="Inserisci il nome..." type="text" id="name" name="name">
         </div>
 
         <!-- Email Address -->
-        <div class="mt-4">
+        <div class="mt-4 text-center py-2">
             <label for="email">
-                Email
+                <h3>
+                    Email
+                </h3>
             </label>
-            <input type="email" id="email" name="email">
+            <input class="form-control me-2" placeholder="Inserisci l'email..." type="email" id="email" name="email">
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
+        <div class="mt-4 text-center py-2">
             <label for="password">
-                Password
+                <h3>
+                    Password
+                </h3>
             </label>
-            <input type="password" id="password" name="password">
+            <input class="form-control me-2" placeholder="Inserisci la password..." type="password" id="password" name="password">
         </div>
 
         <!-- Confirm Password -->
-        <div class="mt-4">
+        <div class="mt-4 text-center py-2">
             <label for="password_confirmation">
-                Conferma Password
+                <h3>
+                   Riinserisci Password
+                </h3>
             </label>
-            <input type="password" id="password_confirmation" name="password_confirmation">
+            <input class="form-control me-2" placeholder="Inserisci nuovamente la password..." type="password" id="password_confirmation" name="password_confirmation">
         </div>
 
-        <div>
-            <a href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <button type="submit">
+        <div class="text-center py-5">
+            <button class="btn btn-primary" type="submit">
                 Register
             </button>
+            <div>
+                <a href="{{ route('login') }}">
+                    {{ __('Already registered?') }}
+                </a>
+            </div>
+
+
+
         </div>
     </form>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,17 +14,24 @@
         <header>
             <nav class="navbar navbar-expand-lg bg-white">
                 <div class="container">
-                    <a class="navbar-brand" href="{{ route('welcomeLoggato.index') }}">Home</a>
+                    <a class="navbar-brand" href="{{ route('welcomeLoggato.index') }}">
+                        <button type="button" class="btn btn-success">Home</button>
+                    </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarText">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('admin.dashboard') }}">Dashboard</a>
+                                <a class="nav-link" href="{{ route('admin.dashboard') }}">
+                                    <button type="button" class="btn btn-info">Dashboard</button>
+                                </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('admin.restaurants.index') }}">Restaurants</a>
+                                <a class="nav-link" href="{{ route('admin.restaurants.index') }}">
+
+                                    <button type="button" class="btn btn-primary">Restaurants</button>
+                                </a>
                             </li>
                         </ul>
                         <form method="POST" action="{{ route('logout') }}">
